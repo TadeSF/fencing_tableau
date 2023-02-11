a27fb6fa2cd2ed72c75bbe99df4cc257acf9398a
Tade Strehk <tade99@gmx.de>
2023-02-11 11:07:36 +0100

    🐞 fix: Dumb

--- wsgi.py
+++ wsgi.py
@@ -2,10 +2,10 @@ import datetime
 import os
 from main import app
 if __name__ == "__main__":
-    app.run()
     if not os.path.exists("start.log"):
         with open("start.log", "w") as f:
             f.write(f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Started\n")
 
     with open("start.log", "a") as f:
         f.write(f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Started")
+    app.run()
