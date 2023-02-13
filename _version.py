0a5cf384641d6a2bb54e016376ad77cc353775a0
Tade Strehk <tade99@gmx.de>
2023-02-13 01:07:37 +0100

    ✨feature: Versioning

--- _version.py
+++ _version.py
@@ -8,10 +8,11 @@ with open('VERSION.txt') as f:
             version_count = line.split(':')[1].strip()
         if line.startswith('commit:'):
             version_commit = line.split(':')[1].strip()
+            version_commit_short = version_commit[:3] + "." + version_commit[-3:]
         if line.startswith('date:'):
             version_date = line.split(':')[1].strip()
 
 
-version_tuple = (version_stage, version_major, version_count, version_commit, version_date)
+version_tuple = (version_stage, version_major, version_count, version_commit_short, version_date)
 
-VERSION = version_stage + '-' + version_major + '.' + version_count + '-' + version_commit + '.' + version_date
+VERSION = version_stage + '-' + version_major + '.' + version_count + '-' + version_commit_short + '.' + version_date
