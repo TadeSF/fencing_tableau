f01c915bb5c015ba34575dd4eefa1ce950a5e84d
Tade Strehk <tade99@gmx.de>
2023-02-20 18:51:34 +0100

    🐞 fix: Git-Webhook

--- main.py
+++ main.py
@@ -1375,7 +1375,6 @@ def handle_webhook():
     try:
         # Execute the update script
         subprocess.call(['/usr/bin/bash', '/home/pi/fencing_tableau/update_server.sh'])
-        
     except Exception as e:
         return 'Error: {}'.format(e), 500
 
