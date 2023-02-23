b2bdd659771c78e32d60340c3bb096cf195c511c
Tade Strehk <tade99@gmx.de>
2023-02-23 14:19:01 +0100

    ✨feature: Flask logging

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,12 @@
 import datetime
+import logging
 import os
 from main import app
 if __name__ == "__main__":
+    handler = logging.FileHandler('flask.log')
+    handler.setLevel(logging.DEBUG)
+    app.logger.addHandler(handler)
+
     if not os.path.exists("start.log"):
         with open("start.log", "w") as f:
             f.write(f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Started\n")
