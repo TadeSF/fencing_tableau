3286c699acb547c2c9305d4ec8e613e5809ab333
Tade Strehk <42637916+TadeSF@users.noreply.github.com>
2023-08-14 20:56:21 +0200

    ✨ feat: New Svelte UI branch: svelte-app

--- frontend/src/lib/components/NextMatch.svelte
+++ frontend/src/lib/components/NextMatch.svelte
@@ -7,14 +7,15 @@
 
 	export let showDetailsButton = false;
 	export let showToggleButton = false;
-	let drawerIsOpen = false;
 
-	$: activeMatchColor = (() => {
-		if (!$userData.next_matches[0]) return { color: '', icon: faBug, shake: false };
+	let prefix = 'http://127.0.0.1:8080';
+
+	$: activeMatchStatus = (() => {
+		if (!$userData.next_matches[0]) return { color: '', icon: 'fa-bug', shake: false };
 		else if ($userData.next_matches[0].ongoing == true)
 			return {
 				color: '',
-				icon: "fa-spin fa-circle-notch",
+				icon: 'fa-spin fa-circle-notch',
 				shake: false
 			};
 		else if (
@@ -70,23 +71,26 @@
 				<i class="fa-solid fa-warning text-xl text-warning" />
 				<h1 class="text-xl font-bold">No matches Found</h1>
 			</div>
-			{:else if $userData.next_matches.length === 0}
+		{:else if $userData.next_matches.length === 0}
 			<div
-			class="flex flex-col sm:flex-row gap-4 justify-center items-center"
-			in:fade={{ delay: 1000 }}
-			out:fade
+				class="flex flex-col sm:flex-row gap-4 justify-center items-center"
+				in:fade={{ delay: 1000 }}
+				out:fade
 			>
-			
 				<i class="fa-solid fa-check-circle text-xl text-success" />
 				<h1 class="text-xl font-bold">All matches finished in this stage.</h1>
 			</div>
 		{:else}
 			<div in:fade={{ delay: 1000 }} out:fade class="flex flex-col sm:flex-row gap-10">
-				<div class="stats stats-vertical sm:stats-horizontal">
+				<div class="stats stats-vertical lg:stats-horizontal">
 					<div class={`stat place-items-center`}>
 						<div class="stat-title">Status</div>
 						<div class="stat-value">
-							<i class={`fa-solid ${activeMatchColor.icon} text-3xl ${activeMatchColor.shake && 'fa-shake'} ${activeMatchColor.color}`} />
+							<i
+								class={`fa-solid ${activeMatchStatus.icon} text-3xl ${
+									activeMatchStatus.shake && 'fa-shake'
+								} ${activeMatchStatus.color}`}
+							/>
 						</div>
 					</div>
 					<div class="stat place-items-center">
@@ -96,20 +100,69 @@
 					<div class="stat place-items-center">
 						<div class="stat-title">Side</div>
 						<div class="stat-value">
-							<div class={`${$userData.next_matches[0].color == "green" && "bg-green-600"} ${$userData.next_matches[0].color == "red" && "bg-red-600"} ${$userData.next_matches[0].color == "" && "bg-slate-200"} w-10 h-10 rounded-full`}></div>
+							<div
+								class={`${$userData.next_matches[0].color == 'green' && 'bg-green-600'} ${
+									$userData.next_matches[0].color == 'red' && 'bg-red-600'
+								} ${
+									$userData.next_matches[0].color == '' && 'bg-slate-200'
+								} w-10 h-10 rounded-full`}
+							/>
 						</div>
 					</div>
 					<div class="stat place-items-center">
 						<div class="stat-title">Opponent</div>
-						<div
-							class="stat-value flex flex-col sm:flex-row gap-2 justify-center items-center"
-						>
+						<div class="stat-value flex flex-col sm:flex-row gap-2 justify-center items-center">
 							<AvatarFlag flag={$userData.next_matches[0].opponent.nationality} size="w-10" />
 							<div class="btn btn-ghost h-fit justify-self-end text-2xl">
 								{$userData.next_matches[0].opponent.name}
 							</div>
 						</div>
 					</div>
+					{#if $userData.allow_fencers_to_input_scores || $userData.allow_fencers_to_start_matches}
+						<div class="stat place-items-center">
+							<div class="stat-title">Options</div>
+							{#if !$userData.next_matches[0].ongoing && $userData.allow_fencers_to_start_matches}
+								<div class="stat-value">
+									<button
+										class="btn btn-accent"
+										on:click={() => {
+											fetch(
+												`${prefix}/api/matches/set-active?tournament_id=${$page.params.tournament}&match_id=${$userData.next_matches[0].id}`,
+												{
+													method: 'POST',
+													headers: { 'Content-Type': 'application/json' },
+													body: JSON.stringify({
+														override_flag: false
+													})
+												}
+											).catch((err) => {
+												console.error(err);
+											});
+										}}
+										disabled={$userData.next_matches[0].ongoing ||
+											$userData.next_matches[0].piste_occupied ||
+											$userData.next_matches[0].piste == 'TBA'}
+									>
+										<i class="fa-solid fa-play" />
+										<span class="ml-2">Start Match</span>
+									</button>
+								</div>
+							{:else if $userData.next_matches[0].ongoing && $userData.allow_fencers_to_input_scores}
+								<div class="stat-value">
+									<button
+										class="btn btn-secondary"
+										on:click={() => {
+											input_score_modal.showModal();
+										}}
+										disabled={!$userData.next_matches[0].ongoing}
+									>
+										<i class="fa-solid fa-pen-to-square" />
+										<span class="ml-2">Input Score</span>
+									</button>
+								</div>
+							{/if}
+						</div>
+					{/if}
 					{#if showDetailsButton}
 						<a class="stat place-items-center" href={`${$page.url.pathname}/matches`}>
 							<div class="stat-title">Details</div>
@@ -120,18 +173,58 @@
 					{/if}
 				</div>
 			</div>
-			{#if showToggleButton}
-				<div
-					class={`${
-						drawerIsOpen ? 'h-auto' : 'h-0 hidden'
-					} flex flex-col justify-center items-center mt-5`}
-				>
-					<slot />
-				</div>
-			{/if}
 		{/if}
 	</div>
 </div>
+{#if $userData.allow_fencers_to_input_scores && $userData.next_matches}
+	<dialog id="input_score_modal" class="modal">
+		<form
+			method="dialog"
+			class="modal-box flex flex-col sm:flex-row gap-3"
+			on:submit={(e) => {
+				fetch(
+					`${prefix}/api/matches/push-score?tournament_id=${$page.params.tournament}&match_id=${$userData.next_matches[0].id}`,
+					{
+						method: 'POST',
+						headers: { 'Content-Type': 'application/json' },
+						body: JSON.stringify({
+							red_score: e.target[0].value,
+							green_score: e.target[1].value
+						})
+					}
+				).catch((err) => {
+					console.error(err);
+				});
+
+				// clear the form
+				e.target[0].value = '';
+				e.target[1].value = '';
+			}}
+		>
+			<input
+				type="text"
+				placeholder={$userData.next_matches[0]?.color != 'red'
+					? $userData.next_matches[0]?.opponent.name
+					: $userData.name}
+				class="input input-bordered bg-red-500 text-white placeholder:text-red-300 w-full"
+				required
+			/>
+
+			<input
+				type="text"
+				placeholder={$userData.next_matches[0]?.color != 'green'
+					? $userData.next_matches[0]?.opponent.name
+					: $userData.name}
+				class="input input-bordered bg-green-600 text-white placeholder:text-green-300 w-full"
+				required
+			/>
+			<button class="btn btn-accent" type="submit">submit</button>
+		</form>
+		<form method="dialog" class="modal-backdrop">
+			<button>close</button>
+		</form>
+	</dialog>
+{/if}
 
 <style lang="scss">
 	.stat-value {
@@ -139,10 +232,9 @@
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		
+
 		.btn {
 			text-transform: none;
 		}
-
 	}
 </style>
--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -11,6 +11,16 @@
 		if ($page.url.searchParams.get('tournament_id')) return true;
 		return false;
 	};
+
+	let searching = true;
+	let fencerFound = false;
+	let fencerName: string;
+	let fencerId: string;
+	let tournamentId: string;
+
+	if ($page.url.searchParams.get('tournament_id')) {
+		tournamentId = $page.url.searchParams.get('tournament_id');
+	}
 </script>
 
 <svelte:head>
@@ -21,7 +31,40 @@
 	<div class="card w-96 bg-base-100 shadow-xl m-4">
 		<div class="card-body">
 			<div class="card-title">Fencer Login</div>
-			<form class="flex flex-col gap-2 my-5">
+			<form
+				class="flex flex-col gap-2 my-5"
+				on:submit={(e) => {
+					e.preventDefault();
+					login_modal.showModal();
+					const prefix = 'http://127.0.0.1:8080';
+					fetch(prefix + '/login-fencer', {
+						method: 'POST',
+						headers: { 'Content-Type': 'application/json' },
+						body: JSON.stringify({
+							tournament_id: e.target[0].value,
+							search: e.target[1].value
+						})
+					})
+						.then((res) => res.json())
+						.then((data) => {
+							searching = false;
+							if (data.success == "Fencer found") {
+								fencerFound = true;
+								fencerName = data.description;
+								tournamentId = data.tournament_id;
+								fencerId = data.fencer_id;
+							} else {
+								fencerFound = false;
+								console.error(data)
+							}
+						})
+						.catch((err) => {
+							console.error(err);
+							searching = false;
+							fencerFound = false;
+						});
+				}}
+			>
 				<input
 					type="text"
 					placeholder="Tournament ID"
@@ -34,8 +77,34 @@
 					placeholder="Name or Number"
 					class="input input-bordered w-full max-w-xs"
 				/>
+				<button type="submit" class="btn btn-primary">Login</button>
 			</form>
 			<a href="/QWCO0Z/1DMHDUNLHI" class="btn btn-secondary">Login</a>
 		</div>
 	</div>
 </div>
+<dialog id="login_modal" class="modal">
+	<form method="dialog" class="modal-box text-center">
+		{#if searching}
+			<div class="flex justify-center align-center">
+				<i class="fa-solid fa-circle-notch fa-spin text-2xl" />
+			</div>
+		{:else if fencerFound}
+			<i class="fa-solid fa-check-circle text-2xl mb-3 text-success" />
+			<h3 class="font-bold text-lg">Fencer Found</h3>
+			<p>{fencerName}</p>
+		{:else}
+			<i class="fa-solid fa-xmark-circle text-3xl mb-3 text-error" />
+			<h3 class="font-bold text-lg">Fencer Not Found</h3>
+			<p>An Error Occured. Please try again or contact the Tournament Office</p>
+		{/if}
+		<div class="modal-action">
+			{#if !searching}
+				<button class="btn">Close</button>
+				{#if fencerFound}
+					<a href={`/${tournamentId}/${fencerId}`} class="btn btn-success">Login</a>
+				{/if}
+			{/if}
+		</div>
+	</form>
+</dialog>
